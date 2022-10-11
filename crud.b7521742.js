92287dacf5ae3cd2e02270ea84b0dc4ccecb21a6
aleksnedo <104201289+aleksnedo@users.noreply.github.com>
2022-10-11 21:44:20 +0000

    Deploying to gh-pages from @ aleksnedo/test-aleks-js@0c1bf88f82cf4f17d2959b8f0539977e062342da 🚀

---
+++ crud.b7521742.js
@@ -0,0 +1,2 @@
+fetch("http://localhost:3000/comics/2").then((o=>o.json())).then(console.log);
+//# sourceMappingURL=crud.b7521742.js.map
---
+++ crud.f9875361.js
@@ -0,0 +1,2 @@
+fetch("http://localhost:3000/comics/2").then((function(o){return o.json()})).then(console.log);
+//# sourceMappingURL=crud.f9875361.js.map
